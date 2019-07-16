5451560a432dfbb56e87d6ae7a8cd6a540e76ac2
tomporvaz <tomporvaz@gmail.com>
2019-07-16 16:56:45 -0400

    Challenge 2 - Added mounted helmet.hidePoweredBy to universal route.

--- myApp.js
+++ myApp.js
@@ -33,6 +33,8 @@ const helmet = require("helmet");
 
 // Use `helmet.hidePoweredBy()``
 
+app.use(helmet.hidePoweredBy({setTo: 'PHP 4.2.0'}));
+
 
 
 /** 3) Mitigate the risk of clickjacking - `helmet.frameguard()` */
